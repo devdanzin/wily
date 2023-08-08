885aebe571e0274ff326c2cc956e6213e3e4db25
devdanzin <74280297+devdanzin@users.noreply.github.com>
2023-08-08 17:01:04 -0300

    Avoid growing annotated.css by repeatedly appending into it.

--- src/annotator.py
+++ src/annotator.py
@@ -201,6 +201,9 @@ def bulk_annotate() -> None:
     reports_dir = Path(__file__).parents[1] / "reports"
     templates_dir = (Path(__file__).parent / "wily" / "templates").resolve()
     shutil.copyfile(templates_dir / "annotated.js", reports_dir / "annotated.js")
+    css_output = reports_dir / "annotated.css"
+    css_output.unlink(missing_ok=True)
+
     for rev_key in state.index[state.default_archiver].revision_keys:
         rev_data = Path(config.cache_path) / "git" / f"{rev_key}.json"
         as_dict = json.loads(rev_data.read_text())
@@ -218,9 +221,8 @@ def bulk_annotate() -> None:
     result = []
     for name, value in styles.items():
         result.append(f".{name} {{ background-color: {value};}}")
-    css_output = reports_dir / "annotated.css"
     with css_output.open("a") as css:
-        css.write("\n".join(result))
+        css.write("\n\n" + "\n".join(result))
 
 
 def annotate_revision(
